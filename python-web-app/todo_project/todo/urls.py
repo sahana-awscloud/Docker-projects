150e2bba6f2380d34d80e146d14f7a9844d47f09
sahana-awscloud <sahanan3012@gmail.com>
2025-09-21 20:18:42 +0530

    Delete python-web-app/todo_project directory

--- python-web-app/todo_project/todo/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name="home"),
-    path('update/<str:pk>/', views.updateTask, name="update"),
-    path('delete/<str:pk>/', views.deleteTask, name="delete"),
-    path('toggle/<str:pk>/', views.toggleTask, name="toggle"),
-]
